9674d2fdc6b446137e764357580323d0775cbfef
John-Pena <jmp1495@gmail.com>
2022-05-11 22:53:47 -0500

    filled User column with table names and validations

--- models/User.js
+++ models/User.js
@@ -9,16 +9,29 @@ class User extends Model {}
 User.init(
   {
     id: {
-
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     username: {
-
+      type: DataTypes.STRING,
+      allowNull: false
     },
-      email: {
-
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
     },
     password: {
-
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
     }
   },
   {
