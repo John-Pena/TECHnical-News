a599b64cf354a3ec0eb8bb50f2bf8a51752a7182
John-Pena <jmp1495@gmail.com>
2022-05-13 15:38:37 -0500

    imported all models to an index to create associations

---
+++ models/index.js
@@ -0,0 +1,10 @@
+// import all models
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+const Vote = require('./Vote');
+
+// create associations
+
+
+module.exports = { User, Post, Comment, Vote };
