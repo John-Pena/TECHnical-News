095d90a7d4ae0a3f7960edca5f0b7a9ac80b6e0b
John-Pena <jmp1495@gmail.com>
2022-05-13 14:47:02 -0500

    filled out the columns for Post model

--- models/Post.js
+++ models/Post.js
@@ -7,16 +7,28 @@ class Post extends Model {}
 Post.init(
   {
     id: {
-
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
     },
     title: {
-
+      type: DataTypes.STRING,
+      allowNull: false
     },
     post_url: {
-
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true
+      }
     },
     user_id: {
-
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id'
+      }
     }
   },
   {
