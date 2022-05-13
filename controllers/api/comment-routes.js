eea9fe18af3cb1c5a2e633fe2d96b31efe349605
John-Pena <jmp1495@gmail.com>
2022-05-13 17:44:32 -0500

    boilerplate set up for comment api-routes

---
+++ controllers/api/comment-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { Comment } = require('../../models');
+
+// Get all comment
+
+
+// post a comment
+
+
+// delete a comment
+
+
+module.exports = router;
