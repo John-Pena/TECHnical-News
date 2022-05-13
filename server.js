363e03b53e20ff9c8a3a5bf2a8b51d0ad965e17d
John-Pena <jmp1495@gmail.com>
2022-05-13 16:32:23 -0500

    ....added router for server.js

--- server.js
+++ server.js
@@ -9,6 +9,8 @@ const sequelize = require('./config/connection');
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(require('./controllers/'));
+
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
 });
