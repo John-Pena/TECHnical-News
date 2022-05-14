f73d455b802eb815209bce5dcc4a7063e26af3d2
John-Pena <jmp1495@gmail.com>
2022-05-14 18:20:47 -0500

    set up session for upvoting to only allow logged in users to vote

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -91,13 +91,16 @@ router.post('/', (req, res) => {
 
 // ability to vote on post
 router.put('/upvote', (req, res) => {
-  // custom static method created in models/Post.js
-  Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
+  // make sure session exists first
+  if (req.session) {
+    // custom static method created in models/Post.js
+    Post.upvote({ ...req.body, user_id: req.session.user_id }, { Vote, Comment, User })
     .then(updatedVoteData => res.json(updatedVoteData))
     .catch(err => {
       console.log(err);
       res.status(500).json(err);
     });
+  }
 });
 
 // update post
