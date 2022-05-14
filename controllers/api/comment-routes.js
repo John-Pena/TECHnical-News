a132a0a1e9f1ef8bf92625772aa87b91a13e6b44
John-Pena <jmp1495@gmail.com>
2022-05-14 18:31:54 -0500

    updated comment-route to only allow logged in users to comment on posts

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -1,48 +1,51 @@
-const router = require('express').Router();
-const { Comment } = require('../../models');
+const router = require("express").Router();
+const { Comment } = require("../../models");
 
 // Get all comment
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
   Comment.findAll()
-  .then(commentData => res.json(commentData))
-  .catch(err => {
+  .then((commentData) => res.json(commentData))
+  .catch((err) => {
     console.log(err);
     res.status(500).json(err);
   });
 });
 
 // post a comment
-router.post('/', (req, res) => {
-  Comment.create({
-    content_text: req.body.comment_text,
-    user_id: req.session.user_id,
-    post_id: req.body.post_id
-  })
-  .then(commentData => res.json(commentData))
-    .catch(err => {
+router.post("/", (req, res) => {
+  // check the session
+  if (req.session) {
+    Comment.create({
+      content_text: req.body.comment_text,
+      user_id: req.session.user_id,
+      post_id: req.body.post_id,
+    })
+    .then((commentData) => res.json(commentData))
+    .catch((err) => {
       console.log(err);
       res.status(400).json(err);
     });
-})
+  }
+});
 
 // delete a comment
-router.delete('/:id', (req, res) => {
+router.delete("/:id", (req, res) => {
   Comment.destroy({
     where: {
-      id: req.params.id
-    }
+      id: req.params.id,
+    },
   })
-    .then(commentData => {
-      if (!commentData) {
-        res.status(404).json({ message: 'No comment found with this id!' });
-        return;
-      }
-      res.json(commentData);
-    })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-})
+  .then((commentData) => {
+    if (!commentData) {
+      res.status(404).json({ message: "No comment found with this id!" });
+      return;
+    }
+    res.json(commentData);
+  })
+  .catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
 
 module.exports = router;
